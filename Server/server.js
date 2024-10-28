c1fb6aabb69386dd2159bc7fdce5b40377fe9805
KhushiMhasange <99010742+KhushiMhasange@users.noreply.github.com>
2024-10-29 01:25:21 +0530

    updated the httpserver creation

--- Server/server.js
+++ Server/server.js
@@ -1,11 +1,16 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
-const httpServer = createServer();
+// const httpServer = createServer();
 
 // const io = new Server(httpServer, {
 //   cors: "http://localhost:5174/",
 // });
+
+const httpServer = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Socket.io server is running.\n'); // Basic response to confirm it's running
+});
 
 const io = new Server(httpServer, {
   cors: {
